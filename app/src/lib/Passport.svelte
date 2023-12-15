da6abc59aa5f85b3137d08af2c93ecdf7e486eca
Fabian Zeiher <fzeiher@gmail.com>
2023-12-15 18:17:48 +0100

    added create functionality

--- app/src/lib/Passport.svelte
+++ app/src/lib/Passport.svelte
@@ -1,18 +1,21 @@
 <script>
+  import { Web3 } from "web3";
+  import ABI from "../assets/ABI.json";
+  import PassportCard from "./PassportCard.svelte";
+  import PassportForm from "./PassportForm.svelte";
+  import QRCode from 'qrcode'
+
   export let tokenId;
   export let activeAcc;
   let edit = false;
 
-  import { Web3 } from "web3";
-  import ABI from "../assets/ABI.json";
-  import { Link, Route } from "svelte-routing";
-  import PassportCard from "./PassportCard.svelte";
-  import PassportForm from "./PassportForm.svelte";
+  let params = new URL(document.location).searchParams;
+  let qrcode = params.get("qrcode") == "true" ? true : false;
 
   const web3 = new Web3(window.ethereum);
   const contract = new web3.eth.Contract(
     ABI,
-    "0x594878fC30ED30a2992eF7bDcD033C20515Aa5de",
+    "0x619b0cC263C1c64aD7cB0b73527b8C1795F4899d",
   );
 
   async function getItemData(uri) {
@@ -32,9 +35,14 @@
       tokenId, updated.name, updated.desc, updated.family, updated.url, updated.img
     ).send({from: activeAcc});
   }
+  
+  async function generateQR(text) {
+    console.log(text);
+    return await QRCode.toDataURL(text);
+  }
 </script>
 
-{#await contract.methods.getPassport(parseInt(tokenId)).call()}
+{#await contract.methods.getPassport(tokenId).call()}
   <article aria-busy="true">loading nft data</article>
 {:then data}
   {#if edit}
@@ -46,6 +54,18 @@
     </article>
   {:else}
     <PassportCard {...data}/>
+    <button class="outline" on:click={() => {qrcode = true}}>Show QR code</button>
     <button class="outline" on:click={() => {edit = true}}>EDIT</button>
   {/if}
 {/await}
+
+<dialog open={qrcode}>
+  <div>
+    {#await generateQR(window.location.origin + window.location.pathname) then qrCodeUri}
+      <img src={qrCodeUri} alt="QrCode"/>
+    {:catch err}
+      <p>Sorry! Something went wrong!</p>
+    {/await}
+    <button on:click={() => {qrcode = false}}>Close</button>
+  </div>
+</dialog>
